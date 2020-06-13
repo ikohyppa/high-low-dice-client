2b5f60a85e9f26603cad6cd5d1197794a8ed2896
ikohyppa <iko.hyppanen@gmail.com>
2020-06-13 10:56:41 +0300

    actions cleanup

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -98,12 +98,9 @@ export function joinRoom(roomId, username) {
 
 // ***** players state actions *****
 
-//let nextPlayerId = 0;
-
 export const addPlayer = user => ({
   type: ADD_PLAYER,
   payload: {
-    //id: ++nextPlayerId, //player IDs will start from 1
     name: user
   }
 });
@@ -112,7 +109,6 @@ export const addNewPlayer = (roomId, user) => ({
   type: ADD_NEW_PLAYER,
   payload: {
     roomId: roomId,
-    //id: ++nextPlayerId,
     name: user
   }
 });
