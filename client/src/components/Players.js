cfc2aca8a49cb42d6d22a749b73bfd62fa1b1101
ikohyppa <iko.hyppanen@gmail.com>
2020-06-23 20:12:59 +0300

    highlighting player rolls if round low or high

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,53 +1,77 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getPlayers } from '../redux/selectors';
+import { getGame, getPlayers } from '../redux/selectors';
 
-class Players extends React.Component {
-  render() {
-    const { players } = this.props;
-    return (
+const Players = props => {
+  const { players } = props;
+  const {
+    roundLows,
+    roundHighs,
+  } = props.game;
+
+  return (
+    <div>
       <div>
-        <div>
-          <table>
-            <tbody>
-              <tr>
-                <th>Player</th>
-                <th>Score</th>
-                <th>Round 1</th>
-                <th>Round 2</th>
-                <th>Round 3</th>
-                <th>Round 4</th>
-                <th>Round 5</th>
-                <th>Round 6</th>
-              </tr>
-              {players && players.length ? (
-                players.map(player => {
-                  return (
-                    <tr key={`player-${player.id}`}>
-                      <td>{player.name}</td>
-                      <td>{player.score}</td>
-                      {player.rolls.map((value, index) => {
+        <table>
+          <tbody>
+            <tr>
+              <th>Player</th>
+              <th>Score</th>
+              <th>Round 1</th>
+              <th>Round 2</th>
+              <th>Round 3</th>
+              <th>Round 4</th>
+              <th>Round 5</th>
+              <th>Round 6</th>
+            </tr>
+            {players && players.length ? (
+              players.map(player => {
+                return (
+                  <tr key={`player-${player.id}`}>
+                    <td>{player.name}</td>
+                    <td>{player.score}</td>
+                    {player.rolls.map((value, index) => {
+                      if (value === roundLows[index]) {
+                        return (
+                          <td className='low' key={index}>
+                            {value}
+                          </td>
+                        );
+                      } else if (value === roundHighs[index]) {
+                        return (
+                          <td className='high' key={index}>
+                            {value}
+                          </td>
+                        );
+                      } else if (value < roundLows[index] || value > roundHighs[index]) {
+                        return (
+                          <td className='lower-or-higher' key={index}>
+                            {value}
+                          </td>
+                        );
+                      } else {
                         return <td key={index}>{value}</td>;
-                      })}
-                    </tr>
-                  );
-                })
-              ) : (
-                <tr>
-                  <td>No players</td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
+                      }
+                    })}
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td>No players</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default connect(
   state => ({
-    players: getPlayers(state)
+    game: getGame(state),
+    players: getPlayers(state),
   }),
   null
 )(Players);
