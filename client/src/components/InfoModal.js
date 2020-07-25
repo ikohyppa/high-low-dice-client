0456b3c2ded816f9d92cba6583e7896202a56836
ikohyppa <iko.hyppanen@gmail.com>
2020-07-26 00:45:35 +0300

    TopBar redesign

---
+++ client/src/components/InfoModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import '../App.css';
+
+const InfoModal = props => {
+  const { show, title, handleClose } = props;
+  return (
+    <Modal
+      show={show}
+      onHide={handleClose}
+      dialogClassName='infoModal'
+      aria-labelledby='contained-modal-title-vcenter'
+      size="sm"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title as='h6' id='contained-modal-title-vcenter'>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{props.children}</Modal.Body>
+    </Modal>
+  );
+};
+
+export default InfoModal;
--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { connect } from 'react-redux';
+import { BsTag, BsInfoCircle, BsClipboard } from 'react-icons/bs';
+import { MdContentCopy, MdInfo } from 'react-icons/md';
+
 import { getRoom } from '../redux/selectors';
+import InfoModal from './InfoModal';
 
 const TopBar = props => {
+  const [showId, setShowId] = useState(false);
   const { roomId, roomName, username } = props.room;
-  const title = 'Wellcome to High-Low Dice';
+  const title = 'Wellcome to High-Low-Dice';
+  const closeRoomId = () => {
+    setShowId(false);
+  };
   return (
-    <Container fluid className='topbar'>
-      <Row>
-        <Col id='title'>{title}</Col>
-        <Col xs={6} id='room'>
-          {roomId && `Room: ${roomName}`}
-        </Col>
-      </Row>
-      <Row>
-        <Col id='player'>{roomId && `${username}`}</Col>
-        <Col xs={6} id='roomid'>
-          {roomId && `RoomId: ${roomId}`}
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container fluid className='topbar'>
+        <Row>
+          <Col id='title'>{title}</Col>
+          <Col id='player'>{username}</Col>
+          <Col id='room'>
+            {roomId && (
+              <>
+                <BsTag onClick={() => setShowId(true)} />
+                {`Room: ${roomName} `}
+              </>
+            )}
+          </Col>
+          <BsInfoCircle id='info' />
+        </Row>
+      </Container>
+
+      {showId && (
+        <InfoModal title='Room ID' show={showId} handleClose={closeRoomId}>
+          <p className='aling-buttons'>
+            {roomId}
+            <MdContentCopy id='icon' tooltip='copy to clipboard' />
+          </p>
+        </InfoModal>
+      )}
+    </>
   );
 };
 
